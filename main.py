6ea74ee8abe4d6834444ba9d96b4386c7448b81f
Karl Dickman <karldickman@gmail.com>
2024-07-04 09:19:01 -0700

    Specify addresses on command line.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from argparse import ArgumentParser
 from os import getenv
 
 from dotenv import load_dotenv
@@ -7,12 +8,17 @@ from dotenv import load_dotenv
 from DistanceMatrixCaller import DistanceMatrixCaller
 
 def main():
+    # Environment variables
     load_dotenv()
     api_key = getenv("DISTANCE_MATRIX_API_KEY")
     if api_key is None:
         raise Exception("Required environment variable DISTANCE_MATRIX_API_KEY not set")
+    # Arguments
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument("address", type = str, help = "The address to geocode")
+    arguments = argument_parser.parse_args()
     caller = DistanceMatrixCaller("https://api-v2.distancematrix.ai", "https://api.distancematrix.ai", api_key)
-    print(caller.geocode("1600 Amphitheatre Parkway, Mountain View, CA", "accurate"))
+    print(caller.geocode(arguments.address, "accurate"))
 
 if __name__ == "__main__":
     main()
