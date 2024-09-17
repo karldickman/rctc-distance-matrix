840fef95dda350a1e4a0916c2ea1a90fa488d4a3
Karl Dickman <karldickman@gmail.com>
2024-09-16 21:01:08 -0700

    Incorporate recruiting classes into the analysis.

--- attendance.R
+++ attendance.R
@@ -13,21 +13,45 @@ fetch.attendance <- function (file.path) {
   )
   data <- read_sheet(
     "https://docs.google.com/spreadsheets/d/18VXvuxgnlPdGizA4prGbejZdAbWws7DwK_CE-u_qdzA/",
+    "Attendance",
     col_types = paste(columns$type, collapse = "")
   )
   write.csv(data, file.path, row.names = FALSE)
   data
 }
 
-process.attendance <- function (data) {
+fetch.roster <- function (file.path) {
+  if (file.exists(file.path)) {
+    return(read_csv(file.path))
+  }
+  data <- read_sheet(
+    "https://docs.google.com/spreadsheets/d/18VXvuxgnlPdGizA4prGbejZdAbWws7DwK_CE-u_qdzA/",
+    "Roster"
+  ) |>
+    select(!`Last event`)
+  write.csv(data, file.path, row.names = FALSE)
+  data
+}
+
+process.attendance <- function (data, roster) {
+  policy.date <- as.Date("2023-09-04")
+  roster <- roster |>
+    group_by(Name) |>
+    summarise(date_joined = min(`Date joined`)) |>
+    mutate(effective_date_joined = as.Date(ifelse(
+      date_joined < policy.date,
+      policy.date,
+      date_joined
+    )))
   data <- data |>
-    filter(Date >= as.Date("2023-09-04")) |>
-    filter(is.na(`Actual?`)) |>
-    filter(!(Attendee %in% c("Allie Shaich", "Ashley Meagher", "Bisrat Tewelde", "Dorothy Davenport", "Emma Gabriel", "Heather Holt", "Heather Nielsen", "Brendy Hale", "Jamie Zamrin", "Jenna Hui", "John Danstrom", "Keenan Rebera", "Makenna Edwards", "Jesse Burns", "Joey Bomber", "Annika Sullivan", "Mark Deligero", "Molly Evjen", "Reagan Ellis", "Tanvir Kalam", "Talia Staiger", "Jacob Goertz", "David Stingle", "Andy Buhler", "Joby Olguin", "Joseph da Fonseca", "Katie Orchard", "Kirk Sutherland", "Kyle Moredock", "Lucah Katauskas", "Patrick Emami", "James Lum", "Will Kirschner", "Gabe Asch", "Katie Rominger")))
+    select(!c(`Deficit/Surplus`, `Last Event`)) |>
+    inner_join(roster, by = join_by(Attendee == Name)) |>
+    filter(Date >= policy.date & Date >= date_joined) |>
+    filter(is.na(`Actual?`))
   totals <- data |>
     group_by(Attendee) |>
-    summarise(n = n(), last_event = max(Date)) |>
-    arrange(n, last_event)
+    summarise(n = n(), last_event = max(Date), effective_date_joined = min(effective_date_joined)) |>
+    arrange(-as.double(effective_date_joined), n, last_event)
   data |>
     left_join(totals) |>
     mutate(Attendee = factor(Attendee, levels = totals$Attendee))
@@ -40,13 +64,15 @@ plot.attendance <- function (data) {
 }
 
 main <- function (argv = c()) {
-  if (length(argv) < 1) {
+  if (length(argv) < 2) {
     cat("Missing required arguments\n")
     return()
   }
   attendance.file.path <- argv[[1]]
-  attendance.file.path |>
-    fetch.attendance() |>
-    process.attendance() |>
+  roster.file.path <- argv[[2]]
+  attendance <- fetch.attendance(attendance.file.path)
+  roster <- fetch.roster(roster.file.path)
+  attendance |>
+    process.attendance(roster) |>
     plot.attendance()
 }
