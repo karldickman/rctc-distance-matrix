9036ab8c8e6572718046d36da58fef098b8d5c35
Karl Dickman <karldickman@gmail.com>
2024-09-19 22:20:14 -0700

    Refine boxplot sorting algorithm.

--- distance_matrix.R
+++ distance_matrix.R
@@ -58,9 +58,10 @@ main <- function(args = c()) {
     group_by(destination) |>
     summarise(
       median_duration_min = median(duration_min),
-      third_quartile = quantile(duration_min, 0.75)
+      third_quartile = quantile(duration_min, 0.75),
+      max_duration_min = max(duration_min)
     ) |>
-    arrange(median_duration_min, third_quartile)
+    arrange(median_duration_min, third_quartile, max_duration_min)
   data$destination <- factor(data$destination, levels = quartiles$destination)
   data |>
     distance.matrix.box.plot() +
