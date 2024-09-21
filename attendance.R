1a4c595b71be3c39e3685a53b2522f7c7ae1afe4
Karl Dickman <karldickman@gmail.com>
2024-09-21 11:03:18 -0700

    Show white background whenever someone is not a member of the team.

--- attendance.R
+++ attendance.R
@@ -49,54 +49,89 @@ process.attendance <- function (data, roster, from) {
       from,
       date_joined
     )))
-  data <- data |>
+  data |>
+    filter(!(Event %in% c("Board meeting", "Board Meeting"))) |>
     select(!c(`Deficit/Surplus`, `Last Event`)) |>
     inner_join(roster, by = join_by(Attendee == Name)) |>
-    filter(Date >= from & Date >= date_joined) |>
+    filter(Date >= from & Date >= date_joined & (Date <= date_left | is.na(date_left))) |>
     filter(from < date_left | is.na(date_left)) |>
-    filter(is.na(`Actual?`))
+    filter(is.na(`Actual?`)) |>
+    mutate(membership_status = ifelse(is.na(date_left), "Current member", "Departed"))
+}
+
+get.team.member.order <- function (data, roster, from) {
   totals <- data |>
     group_by(Attendee) |>
     summarise(
       total_attended = n(),
       last_event = max(Date),
       effective_date_joined = min(effective_date_joined),
-      date_left = max(date_left)) |>
+      date_left = max(date_left)
+    )
+  never.attended <- roster |>
+    filter(!(Status %in% c("Satellite", "Pause"))) |>
+    filter(`Date left` >= from) |>
+    left_join(data, by = join_by(Name == Attendee)) |>
+    filter(is.na(Event) | Date > `Date left`) |>
+    transmute(
+      Attendee = Name,
+      effective_date_joined = as.Date(ifelse(
+        `Date joined` < from,
+        from,
+        `Date joined`
+      )),
+      date_left = `Date left`,
+      total_attended = 0
+    )
+  totals |>
+    bind_rows(never.attended) |>
     mutate(effective_date_left = as.Date(ifelse(is.na(date_left), Sys.Date(), date_left))) |>
     mutate(days_of_membership = as.double(effective_date_left - effective_date_joined) + 1) |>
     mutate(attendance_per_year = total_attended / days_of_membership * 365.24) |>
-    arrange(-as.double(effective_date_joined), attendance_per_year, last_event)
-  data |>
-    left_join(totals) |>
-    mutate(Attendee = factor(Attendee, levels = totals$Attendee), membership_status = ifelse(is.na(date_left), "Current member", "Departed"))
+    arrange(-as.double(effective_date_joined), attendance_per_year, last_event) |>
+    pull(Attendee)
 }
 
 dates.not.on.team <- function (roster, from, to) {
+  had.pause <- roster |>
+    filter(Status == "Pause") |>
+    select(Name, `Date joined`) |>
+    inner_join(select(roster, Name, `Date left`), join_by(Name == Name)) |>
+    group_by(Name) |>
+    summarise(`Date joined` = min(`Date joined`), `Date left` = max(`Date left`))
+  had.no.pause <- roster |>
+    filter(!(Name %in% had.pause$Name))
+  roster <- bind_rows(had.no.pause, had.pause)
   names <- c()
   dates <- c()
+  reasons <- c()
   # Had not joined team
-  had.not.joined.team <- roster |> filter(`Date joined` >= from)
+  had.not.joined.team <- roster|>
+    filter(`Date joined` >= from)
   for (i in 1:nrow(had.not.joined.team)) {
     row <- had.not.joined.team[i,]
     dates.not.joined <- from:(row$`Date joined` - 1)
     for (date in dates.not.joined) {
       names <- c(names, row$Name)
       dates <- c(dates, date)
+      reasons <- c(reasons, "not yet joined")
     }
   }
   # Left team
-  left.team <- roster |> filter(!is.na(`Date left`))
+  left.team <- roster |>
+    filter(`Date left` >= from)
   for (i in 1:nrow(left.team)) {
     row <- left.team[i,]
     dates.left <- row$`Date left`:to
     for (date in dates.left) {
       names <- c(names, row$Name)
       dates <- c(dates, date)
+      reasons <- c(reasons, "left team")
     }
   }
   # Combine joined and left
   dates <- as.Date(dates)
-  tibble(Attendee = names, Date = dates)
+  tibble(Attendee = names, Date = dates, not_on_team_reason = reasons)
 }
 
 plot.attendance <- function (data, from, to) {
@@ -133,8 +168,10 @@ main <- function (argv = c()) {
   to <- Sys.Date()
   not.on.team <- dates.not.on.team(roster, from, to)
   attendance <- fetch.attendance("--cache" %in% argv) |>
-    process.attendance(roster, from) |>
-    bind_rows(not.on.team)
+    process.attendance(roster, from)
+  team.member.order <- get.team.member.order(attendance, roster, from)
+  attendance <- bind_rows(attendance, not.on.team)
   attendance |>
+    mutate(Attendee = factor(attendance$Attendee, levels = team.member.order)) |>
     plot.attendance(from, to)
 }
