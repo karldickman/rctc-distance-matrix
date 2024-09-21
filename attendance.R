ffe05210147cdb80b73c6534e8da467a8664eeb0
Karl Dickman <karldickman@gmail.com>
2024-09-21 06:47:37 -0700

    Specify column types of roster

--- attendance.R
+++ attendance.R
@@ -26,9 +26,14 @@ fetch.roster <- function (cache = FALSE) {
   if (cache & file.exists(file.path)) {
     return(read_csv(file.path))
   }
+  columns <- data.frame(
+    name = c("Name", "Slack status", "Status", "Required?", "Date joined", "Date left", "From", "To", "Days", "Required", "Attended", "RSVPed", "Total days", "Events/day", "Events/week", "Events/month", "Deficit/Surplus", "Last event", "Last reach-out", "Monday Strength", "Wednesday Strength", "Strength Average", "Duniwednesday", "Open Gym", "Foodie Friday", "Team Race", "Social", "Long Run"),
+    type = c("c",    "c",            "c",       "l",        "D",           "D",         "D",    "D",  "d",    "d",        "d",        "d",      "d",          "d",          "d",           "d",            "d",               "D",          "D",              "d",               "d",                  "d",                "d",             "d",        "d",             "d",         "d",      "d")
+  )
   data <- read_sheet(
     "https://docs.google.com/spreadsheets/d/18VXvuxgnlPdGizA4prGbejZdAbWws7DwK_CE-u_qdzA/",
-    "Roster"
+    "Roster",
+    col_types = paste(columns$type, collapse = "")
   ) |>
     select(!`Last event`)
   write.csv(data, file.path, row.names = FALSE)
