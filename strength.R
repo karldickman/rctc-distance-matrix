449ce26eba147306419788ead56e0350758e0b2d
Karl Dickman <karldickman@gmail.com>
2025-01-01 16:13:00 -0800

    Add code that plots the number of strength regulars.

--- strength.R
+++ strength.R
@@ -11,6 +11,29 @@ fetch.strength <- function () {
     filter(!(Date %in% as.Date(c("2024-02-26", "2024-03-04"))))
 }
 
+strength.experience <- function () {
+  strength <- fetch.strength() |>
+    select(!c(`Actual?`, Event, Location, Note, `Date Joined`, `Date Left`, Membership))
+  roster <- fetch.roster() |>
+    group_by(Name) |>
+    summarise(date_joined = min(`Date joined`))
+  start.date <- as.Date("2024-01-01")
+  strength |>
+    inner_join(select(strength, c(Attendee, Date)), join_by(Attendee == Attendee), relationship = "many-to-many") |>
+    filter(Date.y <= Date.x) |>
+    rename(Date = Date.x) |>
+    group_by(Attendee, Date) |>
+    tally() |>
+    arrange(Date, Attendee) |>
+    group_by(Date) |>
+    summarise(proportion_with_required_experience = sum(n >= 24) / n()) |>
+    ggplot(aes(x = Date, y = proportion_with_required_experience)) +
+    geom_point() +
+    geom_smooth() +
+    ggtitle("Development of a group of strength regulars") +
+    ylab("Proportion who have attended a minimum of 24 classes")
+}
+
 main <- function () {
   fetch.strength() |>
     group_by(Date, Event) |>
